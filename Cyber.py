9dda28103a2823e9ebbf7e2f70a4c92850c4b07b
James Tietcheu <jamestietcheu@Jamess-MacBook-Air.local>
2020-05-15 22:15:30 -0400

    updates on basics requirements

--- Cyber.py
+++ Cyber.py
@@ -4,43 +4,53 @@ Created on Thurs May 12 3.09 PM
 @author: James Tietcheu
 
 """
-
+import csv
 import sys
 import os
-import csv, pathlib
-class WebsiteCheck:
+import glob
+from csv import reader
 
-    #def __init__(s):
-    def accessFile(row):
-        user_input = input("Enter the path of your file: ")
+def jsToCsv(dir):
+    print("File extension changed from .js to .csv")
+    for dirpath, dirs, files in os.walk(dir):
+        #print(f"File with .js extension {file_path} changed to", end="")
+        for filename in glob(os.path.join(dirpath, "*.js")):
+                print(filename)
+                try:
+                    pre, ext = os.path.splitext(filename)
+                    print(f"File with .csv extension  {pre + '.csv'}")
+                    dir_path = os.path.join(dir, pre + '.csv')
+                    print(dir_path)
+                except Exception as e:
+                    print(e)
 
-        for path in pathlib.Path(user_input).glob("*.js"):
-            with path.open() as jsfile, path.with_suffix(".csv").open(mode="w") as csvfile:
-                reader = csv.reader(jsfile, delimiter='}')
-                writer = csv.writer(csvfile)
+def main():
+    dir = input("Enter the directory of the file: ")
+    jsToCsv(dir)
+    wordUnigram(dir)
 
-                for row in reader:
-                    writer.writerow(row)
-                    #print(row)
-        return row
-        '''assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
-        f = open(user_input,'r+')
-        print("Your file was found!")
-        # stuff you do with the file goes here
-        f.close()'''
 
-    def wordUnigram(row):
-        count = {}
-        for w in row.read().split(" "):
-            if w in count:
-                count[w] += 1
-            else:
-                count[w] = 1
-        for word, times in count.items():
-            print ("%s was found %d times" % (word, times))
+def wordUnigram(dir):
+
+    count = {}
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            print(file)
+            if file.endswith(".csv"):
+                with open(file, 'r') as csvfile:
+                    reader = csv.reader(csvfile)
+                    rows = [row for row in reader]
+                    print(rows)
+
+                # for w in line:
+                #     if w in count:
+                #         count[w] += 1
+                #     else:
+                #         count[w] = 1
+                #     for word, times in count.items():
+                #         print("%s was found %d times" % (word, times))
+                #         os.path.join(path, filename)
+
 
 if __name__ == '__main__':
-
-    my_website = WebsiteCheck()
-    my_website.accessFile()
-    my_website.wordUnigram()
+    main()
