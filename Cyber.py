60be40cec1d8a57797f35995ee12216df6fe53cc
James Tietcheu <jamestietcheu@Jamess-MacBook-Air.local>
2020-06-03 11:36:09 -0400

    getting the output on csv file

--- Cyber.py
+++ Cyber.py
@@ -13,6 +13,7 @@ import sys
 import os
 import glob
 from csv import reader
+import pandas as pd
 
 import pdb
 
@@ -21,14 +22,19 @@ class website_check():
     def __init__(self, path):
         # pdb.set_trace()
         self.path = path
+        self.final_result = {}
         f = open(path, 'r')
         self.lines = f.read()
+        self.file_length = 0
+        for element in self.lines:
+            self.file_length += 1
         self.lines_words = re.sub(r"[^0-9A-Za-z_]", " ", self.lines)  # getting non-whitespace characters
         self.lines_characters = re.sub(r"[0-9A-Za-z_]", " ", self.lines)  # getting special characters
         f.close()
 
-    def word_unigram(self):
 
+    def word_unigram(self):
+        unigram = 0
         count = {}
         words = self.lines_words.split()
         for w in words:
@@ -36,11 +42,14 @@ class website_check():
                 count[w] += 1
             else:
                 count[w] = 1
+        for key, value in count.items():
+            unigram += value
+        final_unigram = unigram/self.file_length
+        self.final_result['word_unigram'] = final_unigram
         return count
 
-
-
     def keyword_count(self):
+        element = 0
         keyword_list = ['break', 'case', 'catch', 'continue', 'debugger', 'default', 'delete', 'do', 'else', 'finally',
                        'for', 'function', 'if', 'in', 'instanceof', 'new', 'return', 'switch', 'this', 'throw', 'try',
                        'typeof', 'var', 'void', 'while', 'with']
@@ -54,9 +63,15 @@ class website_check():
             if value == 1:
                 unique_keyword.append(key)
 
+        for key, value in count.items():
+            element += value
+        final_element = element/self.file_length
+        self.final_result['keyword_count'] = final_element
+
         return count
 
     def token_count(self):
+        element = 0
         token_list = ['!', '!=', '!==', '%', '%=', '&', '&&', '&=', '(', ')', '*', '*=', '+', '++', '+=', ',', '-', '--',
                       '-=', '.', '..', '...', '/', '/=', ':', '::', ';', '<', '<<', '<<=', '<=', '=', '==', '===', '>',
                       '>=', '>>', '>>=', '>>>', '>>>=', '?', '[', ']', '^', '^=', '{', '|', '|=', '||', '}', '~']
@@ -66,10 +81,16 @@ class website_check():
             if token in words:
                 count[token] = self.lines_characters.count(token)
 
+        for key, value in count.items():
+            element += value
+        final_element = element/self.file_length
+        self.final_result['token_count'] = final_element
+
         return count
 
     # finding unique keywords option 2
     def unique_keywords(self):
+        element = 0
         keyword_list = ['break', 'case', 'catch', 'continue', 'debugger', 'default', 'delete', 'do', 'else', 'finally',
                        'for', 'function', 'if', 'in', 'instanceof', 'new', 'return', 'switch', 'this', 'throw', 'try',
                        'typeof', 'var', 'void', 'while', 'with']
@@ -81,27 +102,48 @@ class website_check():
         for key, values in count.items():
             if values == 1:
                 result.append(key)
+
+        for value in result:
+            element += 1
+        final_element = element/self.file_length
+        self.final_result['unique_keyword'] = final_element
+
         return result
 
     # finding the the length of each line
     def line_average(self):
+        element = 0
         count = {}
         lines = open(self.path, 'r').readlines()
         for line in lines:
             count[line] = len(line)
+
+        for key, value in count.items():
+            element += value
+        final_element = element/self.file_length
+        self.final_result['line_average'] = final_element
+
         return count
 
     # counting the number of comment symbols
     def comment_count(self):
+        element = 0
         comment_list = ['//', '#', '/*', '*/']
         count = {}
         symbols = self.lines_characters.split()
         for comment in comment_list:
             count[comment] = symbols.count(comment)
+
+        for key, value in count.items():
+            element += value
+        final_element = element/self.file_length
+        self.final_result['comment_count'] = final_element
+
         return count
 
     # counting the number of arguments per function
     def parameter_per_function(self):
+        element = 0
         count = {}
         lines = open(self.path, 'r').readlines()
         for line in lines:
@@ -111,10 +153,18 @@ class website_check():
                 l_words = re.sub(r"[^0-9A-Za-z_]", " ", line_updated)   # filtering for all non words and numbers
                 words = l_words.split()
                 count[line] = len(words)    # getting line as a key and the number of arguments of the function as value
+
+        for key, value in count.items():
+            element += value
+        final_element = element/self.file_length
+        self.final_result['parameter_per_function'] = final_element
+
         return count
 
     # determining whether each line start with a whitespace or non-whitespace
     def whitespace_check(self):
+        element = 0
+        data = 0
         result = []
         row = open(self.path, 'r').readlines()
         for line in row:
@@ -122,10 +172,23 @@ class website_check():
                 result.append('non-whitespace')
             if re.match(r'[^0-9A-Za-z_]', line):
                 result.append('whitespace')
+
+        for value in result:
+            if value == 'whitespace':
+                element += 1
+            else:
+                data += 1
+        final_element = element/self.file_length
+        final_data = data/self.file_length
+        self.final_result['whitespace_check'] = final_element
+        self.final_result['non-whitespace_check'] = final_data
+
         return result
 
     # determining whether each line start with a whitespace, tab or new line symbol
     def start_line(self):
+        element = 0
+        data = 0
         result = []
         counting_newline = 0
         lines = open(self.path, 'r').readlines()
@@ -137,10 +200,22 @@ class website_check():
             if re.match(r'\n', line):
                 result.append('new line')
 
+        for value in result:
+            if value == 'new line':
+                element += 1
+            elif value == 'tab':
+                data += 1
+        final_element = element/self.file_length
+        final_data = data/self.file_length
+        self.final_result['new line_check'] = final_element
+        self.final_result['tab_check'] = final_data
+
         #counting the number of empty lines option 1
         for element in result:
             if element == 'new line':
                 counting_newline += 1
+
+
 
         return result
 
@@ -151,11 +226,11 @@ class website_check():
         for line in lines:
             if re.match(r"\n", line):
                 count[line] = lines.count(line)
+
         return count
 
     def empty_character(self):
         lines = open(self.path, 'r').readlines()
-        print(lines)
         i = 0
         word = []
         c = []
@@ -163,7 +238,9 @@ class website_check():
             if lines[i].startswith('\n') and lines[i+1].startswith('}') or lines[i+1].startswith('('):
                 c.append('new line')
             i += 1
-        print(len(c))
+        element = len(c)
+        final_element = element / self.file_length
+        self.final_result['empty_character'] = final_element
         return len(c)
 
     # calculating the average length of each line
@@ -178,14 +255,28 @@ class website_check():
 
     def sd_calc(self):
         data = self.line_average()
-
         mean, stdev = self.avg_calc(), 0.0
         for key, value in data.items():
             stdev += (float(value) - mean) ** 2
         total_item = len(data.keys())
         stdDev = math.sqrt(stdev / float(total_item - 1))
-        print(stdDev)
+
+        self.final_result['Standard_deviation'] = stdDev
+
         return stdDev
+
+    def output_data(self):
+        for key, value in self.final_result.items():
+            output = pd.DataFrame((self.final_result).items(), columns=['element', 'data'])
+        filename = 'result.csv'
+        output.to_csv(filename)
+        result = open(filename, 'r').read()
+        print(result)
+
+
+
+        # for key, value in self.final_result.items():
+        #     print('{} {}'.format(key, value))
 
 
 def main():
@@ -198,40 +289,41 @@ def main():
 
 
 if __name__ == '__main__':
+
     dir_path = main()
     mode = 'r'  # specify the a mode
     directory = os.path.normpath(dir_path)
     for subdir, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".js"):
+                print(file)
                 my_web = website_check(os.path.join(subdir, file))
-
-    # unigram = my_web.word_unigram()
-
-    # unigram = my_web.keyword_count()
-    unigram = my_web.line_average()
-    # unigram = my_web.unique_keywords()
-    # unigram = my_web.token_count()
-    # unigram = my_web.comment_count()
-    # unigram = my_web.empty_character()
-    # unigram = my_web.start_line()
-    # unigram = my_web.empty_line()
-    # unigram = my_web.whitespace_check()
-    # unigram = my_web.avg_calc()
-    # unigram = my_web.sd_calc()
-    # unigram = my_web.parameter_per_function()
+                my_web.word_unigram()
+                my_web.keyword_count()
+                my_web.line_average()
+                my_web.unique_keywords()
+                my_web.token_count()
+                my_web.comment_count()
+                my_web.empty_character()
+                my_web.start_line()
+                my_web.empty_line()
+                my_web.whitespace_check()
+                my_web.avg_calc()
+                my_web.sd_calc()
+                my_web.parameter_per_function()
+                my_web.output_data()
     # lines.close()  # Close file
-    filename = 'result.csv'
-    print('hi: ', unigram)
-    result = open(filename, 'w')
-    for key, value in unigram.items():
-        fieldnames = [key, value]
-        writer = csv.DictWriter(result, fieldnames=fieldnames)
-
-        writer.writeheader()
-        writer.writerow({key : value})
-    #     print(word)
-    #     result.writelines()
-    file = open(filename, 'r').readlines()
+    # filename = 'result.csv'
+    # print('hi: ', unigram)
+    # result = open(filename, 'w')
+    # for key, value in unigram.items():
+    #     fieldnames = [key, value]
+    #     writer = csv.DictWriter(result, fieldnames=fieldnames)
+    #
+    #     writer.writeheader()
+    #     writer.writerow({key : value})
+    # #     print(word)
+    # #     result.writelines()
+    # file = open(filename, 'r').readlines()
     # print(file)
     # print(unigram)
